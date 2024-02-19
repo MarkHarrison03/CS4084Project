084d519ac30947d1eec8a347265179fbc4d4846b
Niomahony <93268576+Niomahony@users.noreply.github.com>
2024-02-19 17:07:22 +0000

    user class updated to include location features

--- app/src/main/java/com/example/reminderapp/User.java
+++ app/src/main/java/com/example/reminderapp/User.java
@@ -6,13 +6,15 @@ public class User {
     private String firstName;
     private String lastName;
     private String userName;
-    ArrayList<Reminder> currentReminders;
+    private ArrayList<Reminder> currentReminders;
+    private ArrayList<Location> userLocations;
 
-    public User(String firstName, String lastName, String userName, ArrayList<Reminder> currentReminders){
+    public User(String firstName, String lastName, String userName, ArrayList<Reminder> currentReminders, ArrayList<Location> userLocations){
         this.firstName = firstName;
         this.lastName = lastName;
         this.userName = userName;
         this.currentReminders = currentReminders;
+        this.userLocations = userLocations;
     }
 
     public ArrayList<Reminder> getCurrentReminders() {
@@ -31,6 +33,10 @@ public class User {
         return firstName;
     }
 
+    public ArrayList<Location> getUserLocations() {
+        return userLocations;
+    }
+
     public void setCurrentReminders(ArrayList<Reminder> currentReminders) {
         this.currentReminders = currentReminders;
     }
@@ -46,11 +52,24 @@ public class User {
     public void setUserName(String userName) {
         this.userName = userName;
     }
+
+    public void setUserLocations(ArrayList<Location> userLocations) {
+        this.userLocations = userLocations;
+    }
+
     public void addUserReminder(Reminder r){
         currentReminders.add(r);
     }
     public void removeReminder(Reminder r){
         currentReminders.remove(r);
         currentReminders.removeIf(Objects::isNull);
+    }
+
+    public void addUserLocation(Location l){
+        userLocations.add(l);
+    }
+    public void removeUserLocation(Location l){
+        userLocations.remove(l);
+        userLocations.removeIf(Objects::isNull);
     }
 }
