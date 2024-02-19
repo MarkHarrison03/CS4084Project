299134d790065dca1fe2eb607d6e9ba482f85acd
MarkHarrison03 <21335265@studentmail.ul.ie>
2024-02-19 14:14:00 +0000

    added user class, fixed reminder package name

--- app/src/main/java/com/example/reminderapp/Reminder.java
+++ app/src/main/java/com/example/reminderapp/Reminder.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.reminderapp;
 import java.util.Date;
 public class Reminder {
     private String description;
---
+++ app/src/main/java/com/example/reminderapp/User.java
@@ -0,0 +1,8 @@
+package com.example.reminderapp;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    public String userName;
+    public Reminder[] currentReminders;
+}
