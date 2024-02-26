dfed6d3c2bf76a9b342cf0f487278a795627cd9b
MarkHarrison03 <21335265@studentmail.ul.ie>
2024-02-26 15:27:29 +0000

    added firebase integration :D

--- app/src/main/java/com/example/reminderapp/MainActivity.java
+++ app/src/main/java/com/example/reminderapp/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainActivity extends AppCompatActivity {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
