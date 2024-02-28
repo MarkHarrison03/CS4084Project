7060ac7abf8c8a2fae554c27bc87ff60d2630adb
Niomahony <93268576+Niomahony@users.noreply.github.com>
2024-02-28 13:22:35 +0000

    LocationReminder/Location/User updates

---
+++ app/src/main/java/com/example/reminderapp/LocationReminder.java
@@ -0,0 +1,33 @@
+package com.example.reminderapp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class LocationReminder extends Reminder {
+    Location location;
+
+    public LocationReminder(String description, String title, LocalDateTime dateInput, boolean canSend, Location location) {
+        super(description, title, dateInput, canSend);
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    //need to add method for checking location against system location and setting can send to true
+    public void activateLocationReminder(LocationReminder locationReminder) {
+        //CODE TO VALIDATE LOCATION WILL GO HERE
+    }
+
+    public void locationReminderParser(ArrayList<LocationReminder> locationReminders, User user) {
+        locationReminders = user.getUserLocationReminders();
+        for (LocationReminder locRem : locationReminders) {
+            activateLocationReminder(locRem);
+        }
+    }
+}
