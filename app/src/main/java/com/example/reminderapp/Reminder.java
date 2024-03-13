d13cc33a72f0d9cfd43cf2ce3b4921adad2373cf
Niomahony <93268576+Niomahony@users.noreply.github.com>
2024-03-13 13:53:40 +0000

    DB pull (broken state)

--- app/src/main/java/com/example/reminderapp/Reminder.java
+++ app/src/main/java/com/example/reminderapp/Reminder.java
@@ -1,82 +1,88 @@
-    package com.example.reminderapp;
+package com.example.reminderapp;
 
-    import com.google.firebase.auth.FirebaseAuth;
-    import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-    import java.lang.reflect.Array;
-    import java.time.*;
-    import java.util.ArrayList;
+import java.lang.reflect.Array;
+import java.time.*;
+import java.util.ArrayList;
 
-    public class Reminder {
-        private String description;
-        private String title;
-        private LocalDateTime dateInput;
-        private String email = Singleton.getInstance().getUserEmail();
-
-
-        public Reminder(String description, String title, LocalDateTime dateInput, String email){
-            this.description = description;
-            this.dateInput = dateInput;
-            this.title = title;
-            this.email = email;
-        }
-
-        public LocalDateTime getDateInput() {
-            return dateInput;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public String getEmail(){
-            return email;
-        }
+public class Reminder {
+    private String description;
+    private String title;
+    private LocalDateTime dateInput;
+    private String email;
 
 
-
-        public void setDateInput(int year, int month, int day, int hour, int minute) {
-            this.dateInput = LocalDateTime.of(year, month, day, hour, minute);
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-
-        public boolean activateReminder(Reminder reminder) {
-            return reminder.dateInput == LocalDateTime.now();
-        }
-
-        public void reminderParser(ArrayList<Reminder> reminders){
-            for (Reminder r:reminders){
-                activateReminder(r);
-            }
-        }
-
-        //Method to get today's reminders and add them to a separate list to save on cpu and memory usage
-        public void todayReminders(ArrayList<Reminder> reminders, User user){
-            LocalDate currentDate = LocalDate.now();
-            reminders = user.getUserReminders();
-            for(Reminder r: reminders){
-                if(r.getDateInput().toLocalDate().equals(currentDate)){
-                    user.addTodayReminder(r);
-                }
-            }
-            //Checking if there are any reminders not for today's date in here and removing them
-            for (Reminder rem: user.getTodayReminders()){
-                if (!(rem.getDateInput().toLocalDate().equals(currentDate))){
-                    user.removeTodayReminder(rem);
-                }
-            }
-        }
-
+    public Reminder(String description, String title, LocalDateTime dateInput) {
+        this.description = description;
+        this.dateInput = dateInput;
+        this.title = title;
+        this.email = Singleton.getInstance().getUserEmail();
     }
+
+    public Reminder() {
+        this.description = "no input";
+        this.title = "no input";
+        this.dateInput = LocalDateTime.now();
+        String email = Singleton.getInstance().getUserEmail();
+    }
+
+    public LocalDateTime getDateInput() {
+        return dateInput;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setDateInput(int year, int month, int day, int hour, int minute) {
+        this.dateInput = LocalDateTime.of(year, month, day, hour, minute);
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public boolean activateReminder(Reminder reminder) {
+        return reminder.dateInput == LocalDateTime.now();
+    }
+
+    public void reminderParser(ArrayList<Reminder> reminders) {
+        for (Reminder r : reminders) {
+            activateReminder(r);
+        }
+    }
+
+    //Method to get today's reminders and add them to a separate list to save on cpu and memory usage
+    public void todayReminders(ArrayList<Reminder> reminders, User user) {
+        LocalDate currentDate = LocalDate.now();
+        reminders = user.getUserReminders();
+        for (Reminder r : reminders) {
+            if (r.getDateInput().toLocalDate().equals(currentDate)) {
+                user.addTodayReminder(r);
+            }
+        }
+        //Checking if there are any reminders not for today's date in here and removing them
+        for (Reminder rem : user.getTodayReminders()) {
+            if (!(rem.getDateInput().toLocalDate().equals(currentDate))) {
+                user.removeTodayReminder(rem);
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/reminderapp/UserReminders.java
+++ app/src/main/java/com/example/reminderapp/UserReminders.java
@@ -4,12 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,12 +16,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class UserReminders extends AppCompatActivity {
 
-    private ArrayList<Reminder> remindersList;
-    private ArrayAdapter<Reminder> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,36 +28,34 @@ public class UserReminders extends AppCompatActivity {
 
         ListView listView = findViewById(R.id.listView);
 
-        // Initialize the remindersList with an empty ArrayList
-        remindersList = new ArrayList<>();
-
         // Initialize Adapter
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, remindersList);
-
-        // Set Adapter
-        databaseQuery();
-
-
-        listView.setAdapter(adapter);
+        ArrayList<Reminder> remindersList = databaseQuery();
+        System.out.println(remindersList);
+//
+//        ArrayAdapter<Reminder> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, remindersList);
+//
+//        listView.setAdapter(adapter);
     }
 
 
-    public void databaseQuery(){
+    public ArrayList<Reminder> databaseQuery(){
+        ArrayList<Reminder> reminderList  = new ArrayList<Reminder>();
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference remindersRef = database.getReference("reminders");
 
         Query query = remindersRef.orderByChild("email").equalTo(Singleton.getInstance().getUserEmail());
+        Log.d("RETRIEVE", "test1");
         query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                remindersList = new ArrayList<>();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Reminder reminder = snapshot.getValue(Reminder.class);
-                    remindersList.add(reminder);
-                }
+                Log.d("RETRIEVE", "test2");
 
-                // Notify the adapter that the data set has changed
-                adapter.notifyDataSetChanged();
+
+                    Reminder reminder = dataSnapshot.getValue(Reminder.class);
+                    Log.d("RETRIEVE", reminder.getTitle());
+                    reminderList.add(reminder);
+
             }
 
             public void onCancelled(@NonNull DatabaseError databaseError) {
@@ -67,5 +63,6 @@ public class UserReminders extends AppCompatActivity {
                 Toast.makeText(UserReminders.this, "Failed to fetch reminders: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+        return reminderList;
     }
 }
