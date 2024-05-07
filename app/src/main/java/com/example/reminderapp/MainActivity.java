7b61fa6834d0e92ceddd18f1225e4398679b8341
MarkHarrison03 <21335265@studentmail.ul.ie>
2024-05-07 14:01:12 +0100

    fixed issues

--- app/src/main/java/com/example/reminderapp/MainActivity.java
+++ app/src/main/java/com/example/reminderapp/MainActivity.java
@@ -220,7 +220,7 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                     });
-        }}}
+        }}
 
 
     private void createNotificationChannel() {
