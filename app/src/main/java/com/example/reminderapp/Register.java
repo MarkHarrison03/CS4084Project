1c2e8c989dd5937185661cf5ce7cd4ca4d94f7de
Brendan Swords <brendanswords21@gmail.com>
2024-05-08 11:00:25 +0100

    made register also log you in

--- app/src/main/java/com/example/reminderapp/Register.java
+++ app/src/main/java/com/example/reminderapp/Register.java
@@ -89,7 +89,7 @@ public class Register extends AppCompatActivity {
                                 }
                             }
                         });
-                
+
             }
         });
     }
